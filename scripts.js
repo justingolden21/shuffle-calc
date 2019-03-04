efa1320bda36c35e7b8fa2528b2d8decf135af5a
RGB Studios <justingolden21@gmail.com>
2019-03-04 02:29:51 -0800

    add shuffle input option

--- scripts.js
+++ scripts.js
@@ -2,5 +2,25 @@ $(function() {
 	$('#num-cards').change(function() {
 		$('#num-shuffles').val(Math.ceil(1.5 * Math.log2(parseInt($(this).val() ) ) ) );
 	});
+
+	$('#shuffle-btn').click(function() {
+		let arr = $('#shuffle-input').val().split(' ').join(',').split(',');
+		arr = arr.filter(function (el) {
+			return el != '';
+		});
+		$('#shuffle-output').val(shuffleArray(arr) );
+	});
+
 	$('#num-cards').val('52').change().select();
 });
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(arr) {
+	for(let i = arr.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1) );
+		let tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	return arr;
+}
