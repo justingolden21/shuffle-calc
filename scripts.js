72846a09e30ab85324c7f5ab526be41d32562cb6
Justin Golden <justingolden21@gmail.com>
2019-10-01 00:30:36 -0700

    add url params, copy btn, and disable output

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
+let removeCopiedText = 0;
+
 $(function() {
 	$('#num-cards').change(function() {
+		$('#num-shuffles').prop('disabled', false);
 		$('#num-shuffles').val(Math.ceil(1.5 * Math.log2(parseInt($(this).val() ) ) ) );
+		$('#num-shuffles').prop('disabled', true);
 	});
 
 	$('#shuffle-btn').click(function() {
@@ -9,7 +13,33 @@ $(function() {
 			return el != '';
 		});
 		$('#shuffle-output').val(shuffleArray(arr) );
+
+		// set url params
+		history.replaceState({}, '', '?q=' + arr);
+
 	});
+
+	$('#copy-url-btn').click( ()=> {
+		let tmp = $('<input type="text">').appendTo(document.body);
+		tmp.val(window.location.href);
+		tmp.select();
+		document.execCommand('copy');
+		tmp.remove();
+
+		$('#copied-url').css('display', 'inline');
+		removeCopiedText++;
+		setTimeout( ()=> {
+			removeCopiedText--;
+			if(removeCopiedText==0)
+				$('#copied-url').css('display', '');
+		}, 3000);
+	});
+
+	//get url params
+	let url = new URL(window.location.href);
+	let q = url.searchParams.get('q');
+	$('#shuffle-input').val(q || '');
+	$('#shuffle-btn').click();
 
 	$('#num-cards').val('52').change().select();
 });
